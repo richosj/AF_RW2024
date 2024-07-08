be6ad0f3d2e36f54f1dec1fccb651983c8333f5f
richosj <okm760596@gmail.com>
2024-07-08 11:41:01 +0900

    ㅇㅇ

---
+++ 03_HTML/gulpfile.babel.js
@@ -0,0 +1,193 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import del from 'del';
+import gulpSass from 'gulp-sass';
+import sass from 'sass';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'autoprefixer';
+import postCss from 'gulp-postcss';
+import rename from 'gulp-rename';
+import browserSync from 'browser-sync';
+import concat from 'gulp-concat';
+import fileInclude from 'gulp-file-include';
+import newer from 'gulp-newer';
+import babelify from 'babelify';
+import bro from 'gulp-bro';
+import minify from 'gulp-minify';
+import cssnano from 'cssnano';
+import cached from 'gulp-cached';
+import imagemin from "gulp-imagemin";
+import log from 'fancy-log';
+import sharp from 'sharp';
+import through2 from 'through2';
+
+const browserSyncInstance = browserSync.create();
+
+const src = './src';
+const dist = './dist';
+const ass = '/assets';
+
+const path_src = {
+  html: src + '/html',
+  css: src + ass + '/css',
+  images: src + ass + '/images',
+  js: src + ass + '/js',
+  fonts: src + ass + '/fonts'
+};
+
+const path_dist = {
+  html: dist,
+  css: dist + ass + '/css',
+  images: dist + ass + '/images',
+  js: dist + ass + '/js',
+  fonts: dist + ass + '/fonts'
+};
+
+// Error handler
+const onErrorHandler = function(error) {
+  console.log(error.toString());
+  this.emit('end');
+};
+
+const clean = () => del([dist], { allowEmpty: true });
+
+const html = () => {
+  return gulp.src([
+    path_src.html + '/**/*.html',
+    '!' + path_src.html + '/**/_*',
+    '!' + path_src.html + '/**/_*/**/*',
+    '!' + path_src.html + '/include/**'
+  ])
+  .pipe(plumber({ errorHandler: onErrorHandler }))
+  .pipe(fileInclude({ prefix: '@@', basepath: '@file', context : {
+    page_main : false,
+    page_name : "회사소개"
+  } })) 
+  .pipe( cached('html') )
+  .pipe(gulp.dest(path_dist.html))
+  .pipe(browserSyncInstance.stream());
+};
+
+const sassOptions = {
+  scss: {
+    outputStyle: 'expanded',
+    indentType: 'tab',
+    indentWidth: 4,
+    precision: 8,
+    sourceComments: true,
+  },
+  postcss: [autoprefixer({ overrideBrowserslist: 'last 2 versions' }), cssnano()]
+};
+
+const css = () => {
+  const sassCompiler = gulpSass(sass);
+  return gulp.src([path_src.css + '/index.scss'], { allowEmpty: true })
+    .pipe(plumber({ errorHandler: onErrorHandler }))
+    //.pipe(cached('css'))
+    .pipe(browserSyncInstance.stream())
+    .pipe(sourcemaps.init())
+    .pipe(sassCompiler(sassOptions.scss).on('error', sassCompiler.logError))
+    .pipe(postCss(sassOptions.postcss))
+    .pipe(rename('styles.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path_dist.css))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(path_dist.css))
+};
+
+const cssReset = () => {
+  const sassCompiler = gulpSass(sass);
+  return gulp.src([path_src.css + '/reset.scss'], { allowEmpty: true })
+    .pipe(plumber({ errorHandler: onErrorHandler }))
+    .pipe(sourcemaps.init())
+    .pipe(sassCompiler(sassOptions.scss).on('error', sassCompiler.logError))
+    .pipe(postCss(sassOptions.postcss))
+    .pipe(rename('reset.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(path_dist.css));
+};
+
+
+
+const js = () => {
+  return gulp.src([
+    path_src.js + '/**/*.js',
+    '!' + path_src.js + '/vendor/**',
+    '!' + path_src.js + '/main.js'
+  ])
+  .pipe(newer(path_dist.js + '/all.js'))
+  .pipe(plumber({ errorHandler: onErrorHandler }))
+  .pipe(sourcemaps.init({ loadMaps: true }))
+  .pipe(bro({
+    transform: [
+      babelify.configure({ presets: ['@babel/preset-env'] }),
+      ['uglifyify', { global: true }]
+    ]
+  }))
+  .pipe(concat('all.js'))
+  .pipe(sourcemaps.write('.'))
+  .pipe(minify({ ext: { min: '.min.js' }, ignoreFiles: ['-min.js'] }))
+  .pipe(gulp.dest(path_dist.js))
+  .pipe(browserSyncInstance.stream());
+};
+
+const vendors = () => {
+  return gulp.src(path_src.js + '/vendor/**')
+  .pipe(gulp.dest(path_dist.js + '/vendor'));
+}
+
+const fonts = () => {
+  return gulp.src(path_src.fonts + '/**/*')
+  .pipe(gulp.dest(path_dist.fonts))
+  .pipe(browserSyncInstance.stream());
+};
+
+const webserver = () => {
+  return browserSyncInstance.init({
+    port: 8080,
+    notify: true,
+    reloadDelay: 50,
+    server: {
+      baseDir: "./dist/"
+    },
+    files: [path_dist.css + '/**/*.css'] // Ensure BrowserSync watches for CSS changes
+  });
+};
+
+
+const image = () => {
+  return gulp.src(`${path_src.images}/**/*`)
+    //.pipe(image())
+    .pipe(gulp.dest(path_dist.images))
+};
+
+// const processImages = () => {
+//   return gulp.src(path_src.images + "/**/*.{jpg,jpeg,png,gif,svg}")
+//     .pipe(through2.obj((file, _, cb) => {
+//       if (!file.isBuffer()) return cb(null, file);
+
+//       sharp(file.contents)
+//         .toBuffer()
+//         .then(data => {
+//           file.contents = data;
+//           cb(null, file);
+//         })
+//         .catch(err => cb(err));
+//     }))
+//     .pipe(gulp.dest(path_dist.images))
+//     .on('end', () => log('Images processed successfully.'))
+//     .on('error', (err) => log.error('Error processing images:', err));
+// };
+
+const watch = () => {
+  gulp.watch([path_src.css + "/**/*.scss"], css);
+  gulp.watch([path_src.js + "/**/*.js"], js);
+  gulp.watch([path_src.html + "/**/*.html"], html);
+  gulp.watch([path_src.images + "/**/*"], image);
+};
+
+const live = gulp.parallel(webserver, watch);
+
+export const cleans = gulp.series(clean);
+export const build = gulp.series(gulp.parallel(html, css, cssReset, js, fonts, image, vendors));
+export const dev = gulp.series(build, live);
---
+++ 03_HTML/src/assets/js/business.js
@@ -0,0 +1,95 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgContainer = document.querySelector('.business__imgs');
+
+    if (!imgContainer) {
+        return; // .business__imgs 요소가 없으면 스크립트를 실행하지 않음
+    }
+
+    // 모든 img 요소를 선택합니다.
+    const imgElements = imgContainer.querySelectorAll('img[data-src]');
+
+    // 로더 표시 함수
+    const showLoader = (img) => {
+        const loader = document.createElement('div');
+        loader.classList.add('loader');
+        loader.innerText = 'Loading...';
+        img.parentNode.appendChild(loader);
+    };
+
+    // 로더 제거 함수
+    const hideLoader = (img) => {
+        const loader = img.parentNode.querySelector('.loader');
+        if (loader) loader.remove();
+    };
+
+    // 이미지 로딩 완료 핸들러
+    const onImageLoad = (event) => {
+        const img = event.target;
+        hideLoader(img);
+    };
+
+    // 이미지 로딩 에러 핸들러
+    const onImageError = (event) => {
+        const img = event.target;
+        hideLoader(img);
+        img.alt = 'Failed to load image';
+    };
+
+    // 각 이미지 요소에 대해 처리합니다.
+    imgElements.forEach(img => {
+        const dataSrc = img.getAttribute('data-src');
+        if (dataSrc) {
+            const newSrc = !document.querySelector('.solution') 
+                ? `./assets/images/business/${dataSrc}` 
+                : `./assets/images/solution/${dataSrc}`;
+
+            // 로더를 표시합니다.
+            showLoader(img);
+
+            // 이벤트 리스너를 추가합니다.
+            img.addEventListener('load', onImageLoad);
+            img.addEventListener('error', onImageError);
+
+            // src 속성을 설정하여 이미지를 로드합니다.
+            img.setAttribute('src', newSrc);
+        }
+    });
+
+    // 모든 버튼 요소를 선택합니다.
+    const buttons = document.querySelectorAll('button[data-trigger="modal"]');
+
+    // 모달 열기 함수
+    const openModal = (modal) => {
+        modal.style.display = 'block';
+    };
+
+    // 모달 닫기 함수
+    const closeModal = (modal) => {
+        modal.style.display = 'none';
+    };
+
+    // 버튼 클릭 이벤트 리스너를 추가합니다.
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const target = button.getAttribute('data-target');
+            const modal = document.querySelector(target);
+
+            if (modal) {
+                openModal(modal);
+
+                // 모달 닫기 버튼 이벤트 리스너를 추가합니다.
+                const closeButton = modal.querySelector('.close');
+                if (closeButton) {
+                    closeButton.addEventListener('click', () => closeModal(modal));
+                }
+
+                // 모달 외부 클릭 시 닫기 이벤트 리스너를 추가합니다.
+                window.addEventListener('click', (event) => {
+                    if (event.target === modal) {
+                        closeModal(modal);
+                    }
+                });
+            }
+        });
+    });
+});
